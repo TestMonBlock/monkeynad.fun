78587ce5abe78023e2e6246806bee4dd16548c7a
TestMonBlock <maidzmahi@gmail.com>
2025-02-26 23:28:10 +1300

    s

--- static/js/App.js
+++ static/js/App.js
@@ -1,32 +1,19 @@
-import "./App.css";
-import {
-    Routes,
-    Route
-} from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import LeaderBoard from "./pages/LeaderBoard";
 import Faqs from "./pages/Faqs";
+import "./App.css";
 
 function App() {
-    return ( <
-        >
-        <
-        Routes >
-        <
-        Route exact path = "/"
-        element = { < Home / >
-        }
-        /> <
-        Route exact path = "/leader-board"
-        element = { < LeaderBoard / >
-        }
-        /> <
-        Route exact path = "/faqs"
-        element = { < Faqs / >
-        }
-        /> <
-        /Routes> <
-        />
+    return (
+        <div>
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/leader-board" element={<LeaderBoard />} />
+                <Route exact path="/faqs" element={<Faqs />} />
+            </Routes>
+        </div>
     );
 }
 
